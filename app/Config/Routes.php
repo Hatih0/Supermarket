aed249a372f3ca9eed9835f5c9b9c2e1cdd882a5
Herman <diam.san.rav@gmail.com>
2026-06-17 10:28:54 +0300

    Create login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'AccueilController::index');
 $routes->get('/Check_Caisse', 'CaisseController::checkCaisse');
 $routes->post('/Valider_Achat', 'AchatControllers::insert_achat');
+$routes->get('/Login', 'LoginController::index');
+$routes->post('/LoginCheck', 'LoginController::checkLogin');
--- app/Controllers/AccueilController.php
+++ app/Controllers/AccueilController.php
@@ -7,6 +7,10 @@ class AccueilController extends BaseController
 
     public function index()
     {
+        $current_user = session('user');
+        if (!$current_user) {
+            return view('login/login');
+        }
         return view('accueil/accueil');
     }
 
